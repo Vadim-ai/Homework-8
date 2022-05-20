96126a70dcb73311fe061930c8db932c85e0d730
SkyPro <vadim.phobia@gmail.com>
2022-05-20 22:08:24 +0300

    Выполнил ДЗ №8 + сложные задания

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,11 +12,25 @@ public class Main {
     int clientOS = 0;
     int deliveryDistance = 50;
     int deliveryDays1;
+    char[] fullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+    String sort = "aabccddefgghiijjkk";
+    double[] salary = new double[30];
+    double min = 100_000;
+    double max = 200_000;
+
 
     // Tasks:
         checkYear(year);
         checkUpdate(currentYear,clientOS,clientDeviceYear);
         deliveryDays1 = calculateDeliveryTime(deliveryDistance);
+        reverseArr(fullName);
+        System.out.println(new String(fullName));
+        sortString(sort);
+        ArrayFullfill(salary,min,max);
+        System.out.println(getRandomDouble(100000, 200000));
+        System.out.println(CalculateAverage(salary));
+        System.out.println(CalculateSum(salary));
+
     }
     public static void checkYear(int year) {
         if (year % 4 != 0 || (year % 100 == 0) && (year % 400 != 0)) {
@@ -55,4 +73,48 @@ public class Main {
         }
         return deliveryDays;
     }
-}
+
+    public static void reverseArr (char[] fullName){
+        for (int i = 0; i < (fullName.length)/2; i++) {
+            char corrector = fullName[i];
+            fullName[i] = fullName[fullName.length - i - 1];
+            fullName[fullName.length -i - 1] = corrector;
+        }
+    }
+
+    public static void sortString(String sort){
+        char [] c = sort.toCharArray();
+        for (int i = 0; i < c.length + 1; i++) {
+            if (c[i] == c[i + 1] && c[i] != c[i + 1 + 1]) {
+                System.out.println("Найден дубль в символе - " + c[i]);
+                break;
+            }
+        }
+    }
+
+    public static double getRandomDouble (double min, double max){
+        Random random = new Random();
+        double result = Math.random()*((max - min) + min);
+        return result;
+        }
+
+    public static void ArrayFullfill (double[]salary, double min, double max){
+        for (int i = 0; i < salary.length; i++) {
+            salary[i] = getRandomDouble(min,max);
+        }
+    }
+
+    public static double CalculateAverage(double[] salary) {
+        double average = CalculateSum(salary)/salary.length;
+        return average;
+    }
+
+    public static double CalculateSum(double[] salary){
+        double sum = 0;
+        for (double value:salary) {
+            sum += value;
+        }
+        return sum;
+    }
+
+    }
